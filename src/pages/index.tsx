d3cfde45bfd11c5d7830a8e6126e193a79088ec0
Julio Cesar <cesarsco15@gmail.com>
2021-06-05 00:15:00 -0300

    feat: add husky mais padroes de commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 export const Home: NextPage = (): JSX.Element => {
   return (
     <div>
-      <h1>Teste Simples</h1>
+      <h1>Next + TypeScript</h1>
     </div>
   )
 }
