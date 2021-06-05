8e60fdab1c52503fe631dd8f473e1e2fc7074d40
Julio Cesar <cesarsco15@gmail.com>
2021-06-04 22:46:57 -0300

    alterando texto do index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next'
 
 export const Home: NextPage = (): JSX.Element => {
-  return <></>
+  return (
+    <div>
+      <h1>Teste Simples</h1>
+    </div>
+  )
 }
 
 export default Home
