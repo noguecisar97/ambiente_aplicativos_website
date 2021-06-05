e84f9b85c95cee91ee11c24ea8b08903d8497762
Julio Cesar <cesarsco15@gmail.com>
2021-06-05 01:41:23 -0300

    refactor: atualizado hooks do husky para commits

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 // .eslintrc.js
+function isTruthy(value) {
+  if (!value) return false;
+  return ['1', 'true'].indexOf(value.toLowerCase()) >= 0;
+}
+
+// Warnings are errors in CI
+var OFF = 'off';
+var ERROR = 'error';
+var WARNING = isTruthy(process.env.CI) ? ERROR : 'warn';
+
 module.exports = {
   root: true,
   env: {
@@ -29,7 +39,6 @@ module.exports = {
       ],
       rules: {
         // We will use TypeScript's types for component props instead
-        'react/prop-types': 'off',
 
         // No need to import React when using Next.js
         'react/react-in-jsx-scope': 'off',
@@ -50,6 +59,22 @@ module.exports = {
         ],
 
         'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
+
+        // Warning não é permitido e nem console logs
+        "comma-dangle": OFF,
+        "eqeqeq": [WARNING, "allow-null"],
+        "import/imports-first": OFF,
+        "indent": [WARNING, 2, {"SwitchCase": 1}],
+        "max-len": [WARNING, 100, 2],
+        "no-console": [WARNING, {"allow": ["warn", "error"]}],
+        "no-debugger": WARNING,
+        "no-fallthrough": WARNING,
+        "no-unreachable": WARNING,
+        "no-unused-vars": [WARNING, {"vars": "all", "args": "none"}],
+        "no-var": ERROR,
+        "prefer-const": WARNING,
+        "react/prop-types": OFF,
+        "semi": [WARNING, "never"],
       },
     },
   ],
